897973034c5f89bd8d0b753294ae337b67288806
Adal <adalberto.luiz.santos.junior@gmail.com>
2024-05-25 22:08:12 -0300

    Addition of NotNull to Professor entity

--- src/main/java/com/proficiency_app/proficiency_api/Professor/Professor.java
+++ src/main/java/com/proficiency_app/proficiency_api/Professor/Professor.java
@@ -17,6 +17,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,9 +31,11 @@ import lombok.Setter;
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Professor extends Data {
     @Column(name = "first_name", nullable = false)
+    @NotNull(message = "Primeiro nome é obrigatório")
     private String firstName;
 
     @Column(name = "last_name", nullable = false)
+    @NotNull(message = "Sobre nome é obrigatório")
     private String lastName;
 
     @Column(name = "name", nullable = false)
@@ -40,12 +43,14 @@ public class Professor extends Data {
 
     @Column(name = "email", nullable = false, unique = true)
     @Email(message = "Invalid email format")
+    @NotNull(message = "Email é obrigatório")
     private String email;
 
-    @Column(name = "code", nullable = false, unique = true)
+    @Column(name = "code", unique = true)
     private String code;
 
     @Column(name = "password", nullable = false)
+    @NotNull(message = "Senha é obrigatória")
     private String password;
 
     @OneToMany(mappedBy = "professor")
